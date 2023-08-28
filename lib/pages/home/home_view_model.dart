af2fbdb62c845d79a22062c5e629123322881e0d
Tran Truyen <tran.dinh.truyen@sun-asterisk.com>
2023-08-28 17:04:36 +0700

    [Modify] Update base loading

--- lib/pages/home/home_view_model.dart
+++ lib/pages/home/home_view_model.dart
@@ -1,9 +1,8 @@
-import 'package:domain/interactor/output/output_observer.dart';
 import 'package:domain/model/movie.dart';
 import 'package:domain/model/tv_series.dart';
 import 'package:domain/usecase/get_latest_movies_use_case.dart';
-import 'package:domain/usecase/get_trending_movies_use_case.dart';
 import 'package:domain/usecase/get_latest_series_use_case.dart';
+import 'package:domain/usecase/get_trending_movies_use_case.dart';
 import 'package:flutter_movie_clean/pages/base/base_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -27,54 +26,51 @@ class HomeViewModel extends BaseViewModel {
   AsyncValue<List<Movie>> get trendingMovies => _trendingMovies;
   AsyncValue<List<Movie>> _trendingMovies = const AsyncValue.loading();
 
-  void getLatestMovies() async {
-    await _getLatestMoviesUseCase.call(
-      const GetLatestMoviesInput(),
-      OutputObserver()
-        ..onSubscribe(() {
-          _latestMovies = const AsyncValue.loading();
-        })
-        ..onSuccess((movies) {
-          _latestMovies = AsyncValue.data(movies);
-        })
-        ..onError((error) {
-          _latestMovies = AsyncValue.error(error, StackTrace.empty);
-        }),
+  void getLatestMovies() {
+    launchUseCase(
+      useCase: _getLatestMoviesUseCase,
+      input: const GetLatestMoviesInput(),
+      onSubscribe: () {
+        _latestMovies = const AsyncValue.loading();
+      },
+      onSuccess: (data) {
+        _latestMovies = AsyncValue.data(data);
+      },
+      onError: (error) {
+        _latestMovies = AsyncValue.error(error, StackTrace.empty);
+      },
     );
-    notifyListeners();
   }
 
-  void getLatestSeries() async {
-    await _getLatestSeriesUseCase.call(
-      const GetLatestSeriesInput(),
-      OutputObserver()
-        ..onSubscribe(() {
-          _latestSeries = const AsyncValue.loading();
-        })
-        ..onSuccess((movies) {
-          _latestSeries = AsyncValue.data(movies);
-        })
-        ..onError((error) {
-          _latestSeries = AsyncValue.error(error, StackTrace.empty);
-        }),
+  void getLatestSeries() {
+    launchUseCase(
+      useCase: _getLatestSeriesUseCase,
+      input: const GetLatestSeriesInput(),
+      onSubscribe: () {
+        _latestSeries = const AsyncValue.loading();
+      },
+      onSuccess: (data) {
+        _latestSeries = AsyncValue.data(data);
+      },
+      onError: (error) {
+        _latestSeries = AsyncValue.error(error, StackTrace.empty);
+      },
     );
-    notifyListeners();
   }
 
-  void getTrendingMovies() async {
-    await _getTrendingMoviesUseCase.call(
-      const GetTrendingMoviesInput(),
-      OutputObserver()
-        ..onSubscribe(() {
-          _trendingMovies = const AsyncValue.loading();
-        })
-        ..onSuccess((movies) {
-          _trendingMovies = AsyncValue.data(movies);
-        })
-        ..onError((error) {
-          _trendingMovies = AsyncValue.error(error, StackTrace.empty);
-        }),
+  void getTrendingMovies() {
+    launchUseCase(
+      useCase: _getTrendingMoviesUseCase,
+      input: const GetTrendingMoviesInput(),
+      onSubscribe: () {
+        _trendingMovies = const AsyncValue.loading();
+      },
+      onSuccess: (data) {
+        _trendingMovies = AsyncValue.data(data);
+      },
+      onError: (error) {
+        _trendingMovies = AsyncValue.error(error, StackTrace.empty);
+      },
     );
-    notifyListeners();
   }
 }
